5eac5392b520a0efea06ba4abcc7bbf37881a896
EC2 Default User <ec2-user@ip-172-31-47-114.ec2.internal>
2024-02-16 03:05:43 +0000

    fixed h2 tag issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1>Phoenix</h2> 
+        <h1>Phoenix</h1> 
         </p>
       </header>
     </div>

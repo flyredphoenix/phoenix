6cbbd344d8d8966ac702d11e542282e6d7ed55e8
EC2 Default User <ec2-user@ip-172-31-47-114.ec2.internal>
2024-02-16 12:05:53 +0000

    Added graphql and storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "phoenix": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "phoenixf2d3386a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "phoenix": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
